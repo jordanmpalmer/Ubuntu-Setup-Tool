4d253a2ee94dea75bfc016c570979946944b6dc8
Jordan Palmer <jordan.m.palmer@boeing.com>
2025-03-22 11:17:45 -0400

    fixing script call in install.sh

--- install.sh
+++ install.sh
@@ -12,37 +12,37 @@ prompt_install() {
 
 # Prompt for Zsh installation
 if prompt_install "Would you like to install Zsh?"; then
-    ./script/zsh-install.sh
+    ./scripts/zsh-install.sh
 fi
 
 # Prompt for Tmux installation
 if prompt_install "Would you like to install Tmux?"; then
-    ./script/tmux-install.sh
+    ./scripts/tmux-install.sh
 fi
 
 # Prompt for Go installation
 if prompt_install "Would you like to install Go?"; then
-    ./script/go-install.sh
+    ./scripts/go-install.sh
 fi
 
 # Prompt for Python installation
 if prompt_install "Would you like to install Python?"; then
-    ./script/py-install.sh
+    ./scripts/py-install.sh
 fi
 
 # Prompt for JavaScript installation
 if prompt_install "Would you like to install JavaScript tools?"; then
-    ./script/js-install.sh
+    ./scripts/js-install.sh
 fi
 
 # Prompt for Rust installation
 if prompt_install "Would you like to install Rust?"; then
-    ./script/rust-install.sh
+    ./scripts/rust-install.sh
 fi
 
 # Prompt for Neovim installation
 if prompt_install "Would you like to install Neovim?"; then
-    ./script/nvim-install.sh
+    ./scripts/nvim-install.sh
 fi
 
 echo "Installation script completed."
